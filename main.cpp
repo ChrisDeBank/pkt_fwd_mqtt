f06cc705195d66b49db5cf132be49dd587bb97cc
ChrisDeBank <cdb3g14@soton.ac.uk>
2017-12-15 12:08:03 +0000

    added MQTT functions

--- main.cpp
+++ main.cpp
@@ -23,6 +23,16 @@
 #include <sys/ioctl.h>
 #include <net/if.h>
 
+#include "stdlib.h"
+#include "MQTTClient.h"
+
+#define ADDRESS     "tcp://localhost:1883"
+#define CLIENTID    "ExampleClientPub"
+#define TOPIC       "boats"
+//#define PAYLOAD     "Hello World!"
+#define QOS         1
+#define TIMEOUT     10000L
+
 using namespace std;
 
 #include "base64.h"
@@ -415,6 +425,13 @@ void receivepacket() {
 			
 			//SEND MESSAGE MQTT HERE
 			
+			pubmsg.payload = message;
+			pubmsg.payloadlen = strlen(message);
+			pubmsg.qos = QOS;
+			pubmsg.retained = 0;
+			
+			MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+			
             //fwrite(b64, sizeof(char), j, stdout);
 
             //char buff_up[TX_BUFF_SIZE]; /* buffer to compose the upstream packet */
@@ -574,6 +591,24 @@ int main () {
 	*/
     printf("Listening at SF%i on %.6lf Mhz.\n", sf,(double)freq/1000000);
     printf("------------------\n");
+	
+	MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+	
 
     while(1) {
 
@@ -594,4 +629,50 @@ int main () {
     return (0);
 
 }
+
+/*#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "MQTTClient.h"
+
+#define ADDRESS     "tcp://localhost:1883"
+#define CLIENTID    "ExampleClientPub"
+#define TOPIC       "boats"
+//#define PAYLOAD     "Hello World!"
+#define QOS         1
+#define TIMEOUT     10000L
+
+int mqtt_send(int argc, char* argv[], char payload[])
+{
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+    int rc;
+
+    MQTTClient_create(&client, ADDRESS, CLIENTID,
+        MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    if ((rc = MQTTClient_connect(client, &conn_opts)) != MQTTCLIENT_SUCCESS)
+    {
+        printf("Failed to connect, return code %d\n", rc);
+        exit(-1);
+    }
+    pubmsg.payload = payload;
+    pubmsg.payloadlen = strlen(payload);
+    pubmsg.qos = QOS;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, TOPIC, &pubmsg, &token);
+    printf("Waiting for up to %d seconds for publication of %s\n"
+            "on topic %s for client with ClientID: %s\n",
+            (int)(TIMEOUT/1000), PAYLOAD, TOPIC, CLIENTID);
+    rc = MQTTClient_waitForCompletion(client, token, TIMEOUT);
+    printf("Message with delivery token %d delivered\n", token);
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+    return rc;
+}*/
+}*/
 
